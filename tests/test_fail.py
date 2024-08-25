2722efb48d11d4bfff875bdf8e5c33c78f80830b
seprojectsms <projectse830@gmail.com>
2024-08-25 23:53:59 +0000

    Added test files

---
+++ tests/test_fail.py
@@ -0,0 +1,4 @@
+from myfile import incrementAndCheckEven
+
+def test_increment_and_check_even_fail():
+    assert incrementAndCheckEven(3) == 1  # This should fail
---
+++ tests/test_pass.py
@@ -0,0 +1,4 @@
+from myfile import incrementAndCheckEven
+
+def test_increment_and_check_even_pass():
+    assert incrementAndCheckEven(3) == 2  # This should pass
